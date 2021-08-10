3080bbadf184cd59d491551f7032884dd4aaaefc
khalizdev <muhnurkhalizz@gmail.com>
2021-08-10 15:44:40 +0700

    updatev1.2

--- src/screen/Listproduk.js
+++ src/screen/Listproduk.js
@@ -13,7 +13,7 @@ export default function Listproduk(props) {
               <Link to={`/produk/${product._id}`}>
                 <h2>{product.name}</h2>
               </Link>
-              <div className="price">{product.price}</div>
+              <div className="price">Rp. {product.price}</div>
             </div>
           </div>
     )
